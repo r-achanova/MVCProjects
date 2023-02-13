c3f60c2d664495dccda17cbb0992188c6a83b9bf
r-achanova <r.achanova@pmg-pernik.com>
2023-02-13 11:17:31 +0200

    add view

---
+++ MVC/MVCGarden/Views/Display.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Views
+{
+   public class Display
+    {
+        public decimal PriceVegitables { get; set; }
+        public decimal PriceFruits { get; set; }
+        public int KgVegitables { get; set; }
+        public int KgFruits { get; set; }
+        public decimal Total { get; set; }
+
+        public Display()
+        {
+            this.PriceVegitables = 0;
+            this.PriceFruits = 0;
+            this.KgVegitables = 0;
+            this.KgFruits = 0;
+            this.GetValues();
+        }
+
+        public void GetValues()
+        {
+            this.PriceVegitables = decimal.Parse(Console.ReadLine());
+            this.PriceFruits = decimal.Parse(Console.ReadLine());
+            this.KgVegitables = int.Parse(Console.ReadLine());
+            this.KgFruits = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowResult()
+        {
+            Console.WriteLine(this.Total);
+        }
+    }
+}
