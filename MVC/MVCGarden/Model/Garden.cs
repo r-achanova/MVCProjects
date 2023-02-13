3c25f7aed9aa26c993df516d8f1d59a3412693b4
r-achanova <r.achanova@pmg-pernik.com>
2023-02-13 11:01:25 +0200

    add model

---
+++ MVC/MVCGarden/Model/Garden.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Model
+{
+   public class Garden
+    {
+        public Garden(decimal priceVegitables, decimal priceFruits, int kgVegitables, int kgFruits)
+        {
+            this.PriceVegitables = priceVegitables;
+            this.PriceFruits = priceFruits;
+            this.KgVegitables = kgVegitables;
+            this.KgFruits = kgFruits;
+        }
+
+        public decimal PriceVegitables { get; set; }
+        public decimal PriceFruits { get; set; }
+        public int KgVegitables { get; set; }
+        public int KgFruits { get; set; }
+
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.KgFruits * this.PriceFruits + this.KgVegitables * PriceVegitables);
+        }
+    }
+}
